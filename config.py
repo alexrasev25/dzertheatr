38ee045561902792f303c5dcd871f56928a8d404
alexrasev25 <alexrasev688@gmail.com>
2025-08-21 14:57:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Токен бота (получить у @BotFather)
-BOT_TOKEN = ""
+BOT_TOKEN = "7437077564:AAHFd2V8ey9QhRaBpvrjdhcf5pe8GoMvZIA"
 
 # URL афиши театра
 THEATER_URL = "https://quicktickets.ru/dzerjinsk-dramteatr"
